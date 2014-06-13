d7e6ecbb0b158ef722845a8cdc0862055672d3e4
Sasa Dukovski <sasa.dukovski@gmail.com>
2014-06-13 12:49:53 +0200

    assignment-3 -- first solution

--- assignments/week-4-assignment-3/src/edu/vuum/mocca/SimpleSemaphore.java
+++ assignments/week-4-assignment-3/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -15,22 +15,28 @@ public class SimpleSemaphore {
      * Define a ReentrantLock to protect the critical section.
      */
     // TODO - you fill in here
+	private ReentrantLock lock;
 
     /**
      * Define a Condition that waits while the number of permits is 0.
      */
     // TODO - you fill in here
+	private Condition notFree;
 
     /**
      * Define a count of the number of available permits.
      */
     // TODO - you fill in here. Make sure that this data member will
     // ensure its values aren't cached by multiple Threads..
+	private volatile int numberOfFreeBalls;
 
     public SimpleSemaphore(int permits, boolean fair) {
         // TODO - you fill in here to initialize the SimpleSemaphore,
         // making sure to allow both fair and non-fair Semaphore
         // semantics.
+    	numberOfFreeBalls = permits;
+    	lock = new ReentrantLock(fair);
+    	notFree = lock.newCondition();
     }
 
     /**
@@ -39,6 +45,15 @@ public class SimpleSemaphore {
      */
     public void acquire() throws InterruptedException {
         // TODO - you fill in here.
+    	lock.lockInterruptibly();
+    	try{
+    		while(numberOfFreeBalls == 0){
+	    		notFree.await();
+	    	}
+    		numberOfFreeBalls -=1;
+    	} finally{
+    		lock.unlock();
+    	}
     }
 
     /**
@@ -47,6 +62,17 @@ public class SimpleSemaphore {
      */
     public void acquireUninterruptibly() {
         // TODO - you fill in here.
+    	lock.lock();
+    	try {
+	    	while(numberOfFreeBalls == 0){
+	    		notFree.await();
+	    	}
+	    	numberOfFreeBalls -=1;
+    	} catch (InterruptedException e) {
+    		
+    	} finally{
+    		lock.unlock();
+    	}
     }
 
     /**
@@ -54,6 +80,15 @@ public class SimpleSemaphore {
      */
     void release() {
         // TODO - you fill in here.
+    	numberOfFreeBalls +=1;
+    	lock.lock();
+    	try {
+    		notFree.signalAll();
+		} catch (Exception e) {
+			
+		} finally {
+			lock.unlock();
+		}
     }
 
     /**
@@ -61,6 +96,6 @@ public class SimpleSemaphore {
      */
     public int availablePermits() {
         // TODO - you fill in here to return the correct result
-    	return 0;
+    	return numberOfFreeBalls;
     }
 }
